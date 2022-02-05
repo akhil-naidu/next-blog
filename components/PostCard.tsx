fcd187429f9f4c30fd159226ab8e0f8f1026656f
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2022-02-05 09:21:42 +0530

    chore: perfect image placement in blog home page

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -5,11 +5,12 @@ import Link from 'next/link';
 const PostCard = ({ post }: any) => {
   return (
     <div className='bg-white shadow-lg rounded-lg p-0 lg:px-2 lg:pt-2 pb-4 mb-6'>
-      <div className='relative overflow-hidden shadow-md pb-80 mb-6'>
+      <div className='relative overflow-hidden shadow-md  mb-6'>
         <img
           src={post.featuredImage.data.attributes.url}
           alt={post.title}
-          className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
+          className='object-center object-cover h-96 w-full shadow-lg rounded-lg lg:rounded-lg '
+          // className='object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg'
         />
       </div>
       <h1 className='transition duration-500 text-center mb-4 cursor-pointer hover:text-pink-600 text-3xl font-semibold'>
